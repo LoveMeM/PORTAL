ffefb9cc8d39cbae6bb72942bf568092c4c6c864
Kenith.Q.Wang (g-mis.cncd02.Newegg) 42210 <kw50@buyabs.corp>
2018-04-11 17:19:26 +0800

    Update:完成左侧样式加载

--- src/views/welcome/welcome.vue
+++ src/views/welcome/welcome.vue
@@ -1,18 +1,27 @@
 <template>
     <div id="welcome">
         <div class="style-wrapper">
-            <div class="style-code" :html="styleCodeTag"></div>
+            <div class="style-code" v-show="false" :html="styleCodeTag"></div>
             <pre :html="highlightCode"></pre>
+        </div>
+        <div class="resume-wrapper">
+            <div :html="resumeContent" v-if="enabledHTML"></div>
+            <pre class="resume-markdown" v-else>{{ resumeContent }}</pre>
         </div>
     </div>
 </template>
 <script>
     import { styles, resume } from '../../resource/welcome'
     import Prism from 'prismjs'
+    import marked from 'marked'
     export default {
         data(){
             return {
-                styleCode:""
+                styleCode:"",
+                enabledHTML:false,
+                markdown:"",
+                delay:60,
+                timer:null
             }
         },
         computed:{
@@ -21,11 +30,41 @@
             },
             highlightCode:function(){
                 return Prism.highlight(this.styleCode,Prism.languages.css)
+            },
+            resumeContent:function(){
+                return this.enabledHTML ? marked(this.markdown) : this.markdown
+            }
+        },
+        mounted(){
+
+        },
+        methods:{
+            showStyles(num,callback){
+                let style = styles[num]
+                let prevLength = 0,length = 0
+                if(!style) return
+                length = styles.filter((item,i)=>i<=num).reduce((result,item)=>{
+                    result += item.length
+                    return result
+                },0)
+                prevLength = length - style.length
+                clearInterval(this.timer)
+                this.timer = setInterval(()=>{
+                    let start = this.styleCode.length - prevLength
+                    let char = style.substring(start, start + 1) || ''
+                    this.styleCode += char
+                    if(this.styleCode.length === length ){
+                        clearInterval(this.timer)
+                        callback && callback()
+                    }
+                    else{
+
+                    }
+                },this.delay)
+            },
+            goBottom(){
+                
             }
         }
     }
 </script>
-<style scoped>
-
-
-</style>
